a7567b9d99d06ab5d4026f0382d125e8d33e535d
Alfred <3555765403@qq.com>
2022-12-19 21:38:50 +0800

    4

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,5 +9,13 @@ public class Test {
  //这是一个 程序的入口
  public static void main(String[] args) {
   System.out.println("gogd");
+  System.out.println("gogd");
+  System.out.println("gogd");
+  System.out.println("gogd");
+  System.out.println("gogd");
+  System.out.println("gogd");
+  System.out.println("gogd");
+  System.out.println("gogd");
+  System.out.println("gogd");
  }
 }
