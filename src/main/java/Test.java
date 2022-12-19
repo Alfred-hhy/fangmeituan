fab564e21a5d4d76949f68fd300d869ff94f73f4
Alfred <3555765403@qq.com>
2022-12-19 21:28:38 +0800

    1

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName: Test
+ * @Description:
+ * @author: Alfred
+ * @date: 2022/12/19  21:27
+ */
+@SuppressWarnings("all")
+public class Test {
+ //这是一个 程序的入口
+ public static void main(String[] args) {
+  System.out.println("gogd");
+ }
+}
